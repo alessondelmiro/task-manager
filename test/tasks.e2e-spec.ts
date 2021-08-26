d24e413f7c82f7a10955aecec340bc68fa52911b
alessondelmiro <alessondelmiro@gmail.com>
2021-08-25 21:29:11 -0300

    add tasks module, controller and service + add unit and e2e tests

--- test/tasks.e2e-spec.ts
+++ test/tasks.e2e-spec.ts
@@ -1,24 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { TasksModule } from '../src/tasks/tasks.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [TasksModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/tasks (GET)', () => {
+    return request(app.getHttpServer()).get('/tasks').expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
